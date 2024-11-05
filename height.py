01b823e9054aac2bd207285209e4a65d679681d0
muhammedazif <muhammedazif098@gmail.com>
2024-11-05 10:26:02 -0800

    first pyton programme

---
+++ height.py
@@ -0,0 +1,11 @@
+print("Wecome abored ! We were expecting you!")
+height= int(input("enter your height\n"))
+
+if height >140 :
+    print("\nYou are welcome ")
+elif height==140:
+    print("\nYou lucky charm")
+else:
+    print("\nFuck you")
+
+print("\nYou and your own")
